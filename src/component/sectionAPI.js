a39731337389cb4f45755e9efd50fac15be83271
webdevjosh <ozzikid@gmail.com>
2020-05-12 10:58:07 -0400

    removing comments

--- src/component/sectionAPI.js
+++ src/component/sectionAPI.js
@@ -4,7 +4,6 @@ const baseUrl = '/api/section'
 // const baseUrl = 'http://localhost:3001/api/section'
 
 const getSection = async (section, load) => {
-    // load(true)
     let result = await axios.get(`${baseUrl}/${section}`)
     return (result.data)
 }
